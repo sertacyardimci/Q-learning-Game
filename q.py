07dd9e737edd5d8dac9a45308fb23dda5fb0ce7b
sertac <33147298+sertacyardimci@users.noreply.github.com>
2019-05-14 13:55:47 +0300

    Display Q matrix to screen

--- q.py
+++ q.py
@@ -11,9 +11,13 @@ class Area:
         self.width = width
         self.height = height
         self.reward = 0
+        self.image = None
 
     def setReward(self, reward):
         self.reward = reward
+
+    def setImage(self, image):
+        self.image = image
 
 
 class Player:
@@ -77,26 +81,45 @@ class Player:
         self.score = self.score + value
 
 
-screenWidth = 800
+screenWidth = 1000
 screenHeight = 600
-color = (255, 100, 0)
+colorOrange = (255, 100, 0)
+colorWhite = (255, 255, 255)
 rectWidth = 100
 rectHeight = 100
 rectSpace = 10
 
+pygame.init()
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+done = False
+is_blue = True
+x = 30
+y = 30
+
+clock = pygame.time.Clock()
+
 #   Images
 playerImage = pygame.image.load('player.png')
+bombImage = pygame.image.load('bomb.png')
+
+#   Font
+font = pygame.font.SysFont("comicsansms",20,1)
 
 
 areaList = list()
 
+#   Create game matrix
 for i in range(0, Area.matrixSize):
     areaList.append(list())
     for j in range(0, Area.matrixSize):
-        area = Area(185 + 100 * i + rectSpace * i, 85 + 100 * j +
-                    rectSpace * j, rectWidth, rectHeight)
+        area = Area(185 + 100 * j + rectSpace * j, 85 + 100 * i +
+                    rectSpace * i, rectWidth, rectHeight)
         areaList[i].append(area)
+
 areaList[1][0].reward = 20
+areaList[0][1].image = bombImage
+
+
 player = Player(areaList[0][0].posX, areaList[0][0].posY, playerImage, 100)
 
 
@@ -105,7 +128,7 @@ Q = []
 for i in range(Area.matrixSize ** 2):
     Q.append([])
     for j in range(4):
-        Q[i].append(0)
+        Q[i].append(float(0))
 Q[0][3] = 10
 
 
@@ -163,19 +186,7 @@ def qFunction(pos, direction):
     v = Q[pos][direction] + 0.3 * (areaList[int(newPos / Area.matrixSize)]
                                    [newPos % Area.matrixSize].reward + 0.8 * max(maxQ) - Q[pos][direction])
     Q[pos][direction] = v
-    print("*********************************")
-    for q in Q:
-        print(q)
 
-
-pygame.init()
-screen = pygame.display.set_mode((screenWidth, screenHeight))
-done = False
-is_blue = True
-x = 30
-y = 30
-
-clock = pygame.time.Clock()
 
 while not done:
     for event in pygame.event.get():
@@ -204,15 +215,41 @@ while not done:
 
     screen.fill((0, 0, 0))
 
+#   Fill matrix color to screen
     for i in range(Area.matrixSize):
         for j in range(Area.matrixSize):
-            pygame.draw.rect(screen, color, pygame.Rect(
+            pygame.draw.rect(screen, colorOrange, pygame.Rect(
                 areaList[i][j].posX, areaList[i][j].posY, areaList[i][j].width, areaList[i][j].height))
             # screen.blit(areaList[i * 4 + j].image, (areaList[i * 4 + j].posX, areaList[i * 4 + j].posY))
 
+#   Display matrix image to screen
+    for i in range(Area.matrixSize):
+        for j in range(Area.matrixSize):
+            if areaList[i][j].image != None:
+                screen.blit(areaList[i][j].image, (areaList[i]
+                                                   [j].posX, areaList[i][j].posY))
+
+#   Display Q matrix to screen
+    iMax = Area.matrixSize ** 2
+    jMax = len(Q[0])
+    leftMargin = 650
+    topMargin = 10
+    sizeW = 70
+    sizeH = 25
+    padding = 10
+    for i in range(iMax):
+        for j in range(jMax):
+            pygame.draw.rect(screen, colorWhite, pygame.Rect(
+                leftMargin + j * padding + j * sizeW, topMargin + i * padding + i * sizeH, sizeW, sizeH))
+            text = font.render(str(Q[i][j])[:4], True, (80, 80, 80))
+            screen.blit(text,
+                        (leftMargin + j * padding + j * sizeW, topMargin + i * padding + i * sizeH))
+
+
+#   Display player to screen
     screen.blit(player.image, (player.posX, player.posY))
 
-    # AI
+#   AI
 
     if player.pos != 10:
         rndDirection = random.randint(0, 3)
